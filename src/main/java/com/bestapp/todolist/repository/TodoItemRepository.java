181d539fa917e81d24d7c771ccedf692a0a35bdc
jcmonkey87 <fuzhonglong@gmail.com>
2023-04-12 23:16:49 +0800

    application settings update

--- src/main/java/com/bestapp/todolist/repository/TodoItemRepository.java
+++ src/main/java/com/bestapp/todolist/repository/TodoItemRepository.java
@@ -1,8 +1,11 @@
 package com.bestapp.todolist.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.bestapp.todolist.POJO.TodoItem;
+
 @Repository
-public class TodoItemRepository {
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
   
 }
