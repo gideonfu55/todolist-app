4d389f384fe7e84b2ec5cda43f34f9a1dcf5fb6c
jcmonkey87 <fuzhonglong@gmail.com>
2023-04-13 22:40:34 +0800

    include comments for checkbox click event listener in js

--- src/main/resources/static/js/todolist.js
+++ src/main/resources/static/js/todolist.js
@@ -1,3 +1,5 @@
+
+// Function to handle checkbox click events to handleItemComplete POST method in TodoItemController:
 function handleCheckboxClick(event) {
   const form = event.target.closest("form");
   form.submit();
